7af723c034f033e6c1b3bdc9450052ce1e9b9768
mohammadne <mohammadne.dev@gmail.com>
2023-04-30 07:50:44 +0330

    feat: update package

--- generate.sh
+++ generate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pbs=$(ls ./pbs)
+pbs=($(ls ./pbs))
 
 for pb in "${pbs[@]}"; do
     basename="${pb%.*}"
-    mkdir -p ./out/"$basename"
+    mkdir -p ./"$basename"
     protoc --proto_path=./pbs \
-        --go_out=./out/"$basename" --go_opt=paths=source_relative \
-        --go-grpc_out=./out/"$basename" --go-grpc_opt=paths=source_relative \
+        --go_out=./"$basename" --go_opt=paths=source_relative \
+        --go-grpc_out=./"$basename" --go-grpc_opt=paths=source_relative \
         "$pb"
 done
